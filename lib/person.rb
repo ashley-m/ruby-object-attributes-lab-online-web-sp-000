a0c5116781aea590fba7919a324b7182d2d7f831
Ashley Miller <asm2fe@virginia.edu>
2019-03-13 04:57:15 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -4,7 +4,7 @@ class Person
     @name
   end
 
-  def name = (new_name)
+  def name= (new_name)
     @name = new_name
   end
 
@@ -12,7 +12,7 @@ class Person
     @job
   end
 
-  def job = (new_job)
+  def job= (new_job)
     @job = new_job
   end
 end
